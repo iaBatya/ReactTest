16fcba1708bc6125bbd1ad1349f4266e1692605d
iaBatya <dmitryajax@gmail.com>
2022-06-05 20:38:33 +0300

    inc and dec for buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button} from 'reactstrap'; 
+
+
+
+
 
 function App() {
+  
+  const [selected, setSelected] = useState(0)
+
+
+  function inc() {
+    setSelected( selected + 1)
+  }
+  
+  function dec() {
+    setSelected( selected - 1)
+  }
+  
+  
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table
+      bordered
+      hover
+      >
+        <thead>
+          <tr>
+            <th>
+                Category ^
+            </th>
+            <th>
+                Name
+            </th>
+            <th>
+                Price ^
+            </th>
+            <th>
+                Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">
+                Aaaa
+            </th>
+            <td>
+                aaaa
+            </td>
+            <td>
+                aaaa
+            </td>
+            <td>
+                <Button onClick={dec}>(-)</Button>
+                 <span> Select </span> 
+                <Button onClick={inc}>(+)</Button>
+                <span> {selected} </span>
+            </td>
+            </tr>
+          <tr>
+            <th scope="row">
+                aaa
+            </th>
+            <td>
+                aaa
+            </td>
+            <td>
+                aaaa
+            </td>
+            <td>
+                <Button>(-)</Button>
+                 <span> Select </span> 
+                <Button>(+)</Button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+                aaaa
+            </th>
+            <td selected={false}>
+                aaa
+            </td>
+            <td>
+                aaa
+            </td>
+            <td>
+                <Button>(-)</Button>
+                 <span> Select </span> 
+                <Button>(+)</Button>
+            </td>
+          </tr>
+        </tbody>
+    </Table>
   );
 }
 
